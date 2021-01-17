eb8b08f7f916c763811ad784e946c25d8961af48
Fabrizio Condori Guzman <fabrizio.condori.guzman@gmail.com>
2021-01-17 00:03:55 -0500

    project's base done

---
+++ resources/assets/js/components/AppComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+    export default {
+        created() {
+
+            if(localStorage.token) {
+                axios.get('/api/user', {
+                    headers: {
+                        Authorization: 'Bearer ' + localStorage.getItem('token')
+                    }
+                },
+                ).then(response => {
+                    store.commit('loginUser')
+                }).catch(error => {
+                    if (error.response.status === 401 || error.response.status === 403) {
+                        store.commit('logoutUser')
+                        localStorage.setItem('token', '')
+                        this.$router.push({name: 'login'})
+                    }
+
+                });
+            }
+
+        }
+    }
+</script>
---
+++ resources/assets/js/routes.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import LoginComponent from './components/LoginComponent'
+import LogoutComponent from './components/LogoutComponent'
+import store from './store'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        redirect: { name: 'login' }
+    },
+    /*{
+        path: '/dashboard',
+        name: 'dashboard',
+        component: DashboardComponent,
+        meta: { requiresAuth: true }
+    },*/
+    {
+        path: '/login',
+        name: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '/logout',
+        name: 'logout',
+        component: LogoutComponent
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+
+    // check if the route requires authentication and user is not logged in
+    if (to.matched.some(route => route.meta.requiresAuth) && !store.state.isLoggedIn) {
+        // redirect to login page
+        next({ name: 'login' })
+        return
+    }
+
+    // if logged in redirect to dashboard
+    if(to.path === '/login' && store.state.isLoggedIn) {
+        next({ name: 'dashboard' })
+        return
+    }
+
+    next()
+})
+
+export default router
