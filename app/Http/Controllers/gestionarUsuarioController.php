ae6cca233b675fe5b2d29a1318930f1d19f065f3
Maritza Vega <2016200251@untels.edu.pe>
2021-01-24 22:01:06 -0500

    Cambios en gestionarUsuarios.php,update and delete

--- app/Http/Controllers/gestionarUsuarioController.php
+++ app/Http/Controllers/gestionarUsuarioController.php
@@ -39,7 +39,6 @@ class gestionarUsuarioController extends Controller
 
         $ValidacionCampos = isNullEmpty($req->numDoc,'numDoc') ?: 
                             isNullEmpty($req->rol,'rol') ?:
-                            isNullEmpty($req->usuario,'usuario') ?: 
                             isNullEmpty($req->contrasenia,'contrasenia') ?:
                             isNullEmpty($req->nombres,'nombres') ?:
                             isNullEmpty($req->apellido,'apellido') ?: 
@@ -92,46 +91,57 @@ class gestionarUsuarioController extends Controller
 
       return mySQLInsert(
           "INSERT INTO usuarios (documentos_id_documentos, rol_id_rol, usuario, contrasenia, nombres, apellido, num_documento, direccion, telefono, fechaInicio) 
-                         VALUES ('{$req->tipoDoc}', '{$req->rol}', '{$req->usuario}', '{$req->contrasenia}','{$req->nombres}', '{$req->apellido}','{$req->numDoc}', '{$req->direccion}', '{$req->telefono}',/*'A',*/ '{$req->fechaInicio}')");
+                         VALUES ('{$req->tipoDoc}', '{$req->rol}', '{$req->numDoc}', '{$req->contrasenia}','{$req->nombres}', '{$req->apellido}','{$req->numDoc}', '{$req->direccion}', '{$req->telefono}',/*'A',*/ '{$req->fechaInicio}')");
     }
 
     function deleteUsuario (Request $req){
-           return mySQLDelete("DELETE FROM usuarios WHERE num_documento = '{$req->numDoc}'");
+        $ValidacionCampos = isNullEmpty($req->numDoc);
+        
+        if($ValidacionCampos){
+            return $ValidacionCampos;
+        }
+
+           return mySQLDelete("UPDATE usuarios SET estado = 'I' WHERE num_documento = '{$req->numDoc}'");
     }
 
     function modificarUsuario (Request $req){
-       
-        $TIPO_DNI = 1;
-
-        $ValidacionCampos = isNullEmpty($req->nombre,'digite correctamente el nombre')?: 
-                            isNullEmpty($req->apellido,'digite correctamente el apellido') ?: 
-                            isNullEmpty($req->telefono,'digite correctamente el telefono') ?:
-                            isNullEmpty($req->rol,'digite correctamente el rol') ?:
-                            isNullEmpty($req->contrasenia,'digite correctamente el contraseñia');
+ 
+        $ValidacionCampos = isNullEmpty($req->nombre,'','digite correctamente el nombre')?: 
+                            isNullEmpty($req->apellido,'','digite correctamente el apellido') ?: 
+                            isNullEmpty($req->direccion,'','digite correctamente direccion') ?:
+                            isNullEmpty($req->telefono,'','digite correctamente el telefono') ?:
+                            isNullEmpty($req->rol,'','digite correctamente el rol') ?:
+                            isNullEmpty($req->contrasenia,'','digite correctamente el contraseñia') ?:
+                            isNullEmpty($req->numDoc,'','digite correctamente el contraseñia');
                             
                     
         if($ValidacionCampos){
             return $ValidacionCampos;
         }
-
-        // validacion para campos de dni = 8
-
-       /* if (strlen($req->numDoc) == 8 && $req->tipoDoc == $TIPO_DNI ) { // TIPO 
+        
+        if (strlen($req->contrasenia) < 5) {
             return JSON_ENCODE(
                 (object) [
-                    'status' => $_SESSION["STATUS_CONTROL"],
-                    'msj'    => 'La contraseña debe tener minimo 8 caracteres sí es un DNI.'
+                    'status' => $_SESSION["STATUS_CONTROL"], //status del back
+                    'msj'    => 'La contraseña debe tener minimo 5 caracteres.' //
                 ]
             ); 
-           
-        }*/
-         
-        return mySQLUpDate("UPDATE usuarios SET nombres ='{$req->nombre}', apellidos ='{$req->apellido}', 
-        telefono ='{$req->telefono}', rol_id_rol ='{$req->rol}',contrasenia = '{$req->contrasenia}'
-        WHERE num_documento = '{$req->numDoc}' ");
-                  
- }
+    }
+    
+    if (strlen($req->telefono) <> 9) {
+        return JSON_ENCODE(
+            (object) [
+                'status' => $_SESSION["STATUS_CONTROL"], //status del back
+                'msj'    => 'el telefono debe tener minimo 9 caracteres.' //
+            ]
+        ); 
+    }
 
+        return mySQLUpDate("UPDATE usuarios SET nombres ='{$req->nombre}', apellidos ='{$req->apellido}', 
+        telefono ='{$req->telefono}', rol_id_rol ='{$req->rol}',contrasenia = '{$req->contrasenia}',direccion = '{$req->dirreccion}'
+        WHERE num_documento = '{$req->numDoc}' ");
+          
+ }
 }
 
 
