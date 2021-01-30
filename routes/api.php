d0b2332eb62346f523c174a15512a639f7238196
angelhr <angelhuamannahui@gmail.com>
2021-01-29 22:46:56 -0500

    message

--- routes/api.php
+++ routes/api.php
@@ -3,37 +3,35 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 //initialController
 Route::get('getLogin'         , 'initialController@getLogin');
 Route::post('registrarUsuario', 'initialController@registrarUsuario')->middleware('validateAuth');
 
 //reclamosController
-Route::get('getReclamos', 'reclamosController@getReclamos')->middleware('validateAuth');
+Route::get('getReclamos'   , 'reclamosController@getReclamos'     )->middleware('validateAuth');
 Route::post('postRegistrar', 'reclamosController@postRegistrar')->middleware('validateAuth');
 
 //reporteInvController-Listo para probar (Font)
-Route::get('getEmitirRI','reporteInvController@getEmitirRI' )->middleware('validateAuth');
-Route::get('totalProdCosto','reporteInvController@totalProdCosto')->middleware('validateAuth');
+Route::get('getEmitirRI'   , 'reporteInvController@getEmitirRI'   )->middleware('validateAuth');
+Route::get('totalProdCosto', 'reporteInvController@totalProdCosto')->middleware('validateAuth');
 
 //gestionarUsuarioController-Listo para Probar (Font)
-Route::get('listaUsuario', 'gestionarUsuarioController@listaUsuario')->middleware('validateAuth');
-Route::get('getBuscarUsuario', 'gestionarUsuarioController@getBuscarUsuario')->middleware('validateAuth');
-Route::post('crearUsuario', 'gestionarUsuarioController@crearUsuario')->middleware('validateAuth');
-Route::post('deleteUsuario','gestionarUsuarioController@deleteUsuario')->middleware('validateAuth');
-Route::post('modificarUsuario','gestionarUsuarioController@modificarUsuario')->middleware('validateAuth');
-Route::get('detalleUsuario','gestionarUsuarioController@detalleUsuario');
-
+Route::get('listaUsuario'     , 'gestionarUsuarioController@listaUsuario'    )->middleware('validateAuth');
+Route::get('getBuscarUsuario' , 'gestionarUsuarioController@getBuscarUsuario')->middleware('validateAuth');
+Route::post('crearUsuario'    , 'gestionarUsuarioController@crearUsuario'    )->middleware('validateAuth');
+Route::post('deleteUsuario'   , 'gestionarUsuarioController@deleteUsuario'   )->middleware('validateAuth');
+Route::post('modificarUsuario', 'gestionarUsuarioController@modificarUsuario')->middleware('validateAuth');
+Route::get('detalleUsuario'   , 'gestionarUsuarioController@detalleUsuario'  )->middleware('validateAuth');
+Route::get('detalleUsuario'   , 'gestionarUsuarioController@cmbTipoDoc'      )->middleware('validateAuth'); // trae los combos de tipo dni
 
 //gProductosController
-Route::get('listProduct','gProductosController@listProduct')->middleware('validateAuth');
-Route::post('deleteP','gProductosController@deleteP')->middleware('validateAuth');
-Route::post('updateP','gProductosController@updateP')->middleware('validateAuth');
-Route::post('registrarP','gProductosController@registrarP')->middleware('validateAuth');
-
+Route::get('listProduct', 'gProductosController@listProduct')->middleware('validateAuth');
+Route::post('deleteP'   , 'gProductosController@deleteP'    )->middleware('validateAuth');
+Route::post('updateP'   , 'gProductosController@updateP'    )->middleware('validateAuth');
+Route::post('registrarP', 'gProductosController@registrarP' )->middleware('validateAuth');
 
 //no probar esta en proceso 
 //emitirBvController
-Route::get('listaNotaV','emitirBvController@listaNotaV')->middleware('validateAuth');
-Route::get('buscaNotaVByFechas','emitirBvController@buscaNotaVByFechas')->middleware('validateAuth');
-Route::post('registrarPago','emitirBvController@registrarPago')->middleware('validateAuth');
+Route::get('listaNotaV'        , 'emitirBvController@listaNotaV'        )->middleware('validateAuth');
+Route::get('buscaNotaVByFechas', 'emitirBvController@buscaNotaVByFechas')->middleware('validateAuth');
+Route::post('registrarPago'    , 'emitirBvController@registrarPago'     )->middleware('validateAuth');
