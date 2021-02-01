98ec4a406551cfde207b60ca1821ac2e9fbe2eaf
angelhr <angelhuamannahui@gmail.com>
2021-01-31 22:31:00 -0500

    message

--- app/Http/Controllers/reporteInvController.php
+++ app/Http/Controllers/reporteInvController.php
@@ -10,17 +10,28 @@ class reporteInvController extends Controller
 {
 // lista productos
     function getEmitirRI(Request $req){
-        $isValidate = isNullEmpty($req->fecha);
-        if($isValidate){
-            return $isValidate;
-        }
+        $TIPO_ALTO = 0;
+        $TIPO_BAJO = 1;
+        
         //// el estado 1 es alto en productos y el estado 0 es bajo en productos
-        return mySQLConsulta("SELECT id_producto,nombre,precio,stock,(CASE  
-        WHEN estado = 1 THEN 'ALTO'
-        ELSE CASE WHEN estado = 0 THEN 'BAJO'
-        END 
-        END ) AS estado FROM
-        producto WHERE fecha_mod <= '{$req->fecha}' LIMIT 0,50");
+        return mySQLConsulta(
+            "SELECT id_producto,
+                    nombre,
+                    precio,
+                    stock,
+                    (CASE WHEN stock > 10 
+                          THEN 'ALTO'
+                     ELSE 'BAJO' 
+                    END) AS estado 
+               FROM producto 
+              WHERE (CASE WHEN '{$req->tipoStock}' = '{$TIPO_ALTO}'  
+                          THEN stock > 10
+                          WHEN '{$req->tipoStock}' = '{$TIPO_BAJO}'
+                          THEN stock <= 10  
+                          ELSE 1 = 1 
+                    END)
+              LIMIT 0,50"
+        );
         
     }
 
