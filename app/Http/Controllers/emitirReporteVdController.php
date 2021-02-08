bb173ed6949a968de1512ac2234db6d720945323
angelhr <angelhuamannahui@gmail.com>
2021-02-07 21:47:58 -0500

    cod

--- app/Http/Controllers/emitirReporteVdController.php
+++ app/Http/Controllers/emitirReporteVdController.php
@@ -11,16 +11,19 @@ class emitirReporteVdController extends Controller
     //---Lista de Boletas de Ventas
     function listaBoletaE (Request $req){
 
-    return mySQLConsulta("SELECT b.idB_boleta AS N,
-    b.codigo_boleta AS Codigo,
-    nv.nombre_cliente AS NombreCliente,
-    b.monto AS Monto,
-    t.tipo AS TipodePago
-    FROM boleta AS b, 
-    notadeventas AS nv ,
-    tipopago AS t
-    WHERE t.id_tipopago = b.TIPOPAGO_id_tipopago
-    AND b.NOTADEVENTAS_id_boletaventa = nv.id_boletaventa");
+    return mySQLConsulta(
+      "SELECT b.idB_boleta AS N,
+              b.codigo_boleta AS Codigo,
+              nv.nombre_cliente AS NombreCliente,
+              b.monto AS Monto,
+              t.tipo AS TipodePago
+         FROM boleta AS b, 
+              notadeventas AS nv ,
+              tipopago AS t
+        WHERE t.id_tipopago = b.TIPOPAGO_id_tipopago
+          AND b.NOTADEVENTAS_id_boletaventa = nv.id_boletaventa
+          AND b.nota
+          ");
 
     }
 
@@ -28,21 +31,31 @@ class emitirReporteVdController extends Controller
     // cumple para btn emitir boleta de venta 
     function emitirRBVbyFecha (Request $req){
          
-         return mySQLConsulta("SELECT b.idB_boleta AS N,
-         b.codigo_boleta AS Codigo,
-         nv.nombre_cliente AS NombreCliente,
-         b.monto AS Monto,
-         t.tipo AS TipodePago
-    FROM boleta AS b, 
-         notadeventas AS nv ,
-         tipopago AS t
-   where t.id_tipopago = b.TIPOPAGO_id_tipopago
-     and b.NOTADEVENTAS_id_boletaventa = nv.id_boletaventa
-     AND (CASE WHEN '{$req->fecha}' IS NOT NULL AND  '{$req->fecha}' <> ''
-              THEN b.fecha = DATE_FORMAT('{$req->fecha}', '%Y-%m-%d')
-              ELSE b.fecha = DATE(NOW())
-              END)");
-              
+         
+      $validacionI = isNullEmpty($req->fecha,'','emisor')?:
+      isNullEmpty($req->receptor,'','receptor')?:
+      isNullEmpty($req->mensaje,'','FALTA incidencia');
+
+      if($validacionI){
+        return $validacionI;
+      }
+
+      return mySQLConsulta(
+          "SELECT b.idB_boleta AS N,
+                  b.codigo_boleta AS Codigo,
+                  nv.nombre_cliente AS NombreCliente,
+                  b.monto AS Monto,
+                  t.tipo AS TipodePago
+              FROM boleta AS b, 
+                  notadeventas AS nv ,
+                  tipopago AS t
+            WHERE t.id_tipopago = b.TIPOPAGO_id_tipopago
+              AND b.NOTADEVENTAS_id_boletaventa = nv.id_boletaventa
+              AND (CASE WHEN '{$req->fecha}' IS NOT NULL AND  '{$req->fecha}' <> ''
+                        THEN b.fecha = DATE_FORMAT('{$req->fecha}', '%Y-%m-%d')
+                        ELSE b.fecha = DATE(NOW())
+                        END)"
+      );                  
     }
 
     //---insert de incidecia 
